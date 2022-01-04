49a93000572439b306159031f4070fc4deea7845
Pi Fan <390049717@qq.com>
2022-01-04 21:30:55 +0000

    read

--- src/sort.go
+++ src/sort.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
+import (
+    "os"
+    "fmt"
+    "log"
+)
+
+func read(path string) {
+    file, err := os.Open(path)
+    if err != nil {
+        fmt.Println("os open err: ", err)
+        panic("")
+    }
+
+
+    buffer := make([]byte, 100)
+    data, err := file.Read(buffer)
+    if err != nil {
+        panic("read file err")
+    }
+    fmt.Printf("%d bytes: %s \n", data, string(buffer))
+}
 
 func main() {
-	/*
+    read("src/example1.dat")
+
+    return
+
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	if len(os.Args) != 3 {
@@ -11,9 +34,4 @@ func main() {
 	}
 
 	log.Printf("Sorting %s to %s\n", os.Args[1], os.Args[2])
-
-	 */
-
-	fmt.Println("j")
-	
 }
