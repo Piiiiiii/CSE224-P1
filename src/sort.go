66fecd9f4571cd09b08457b493e06564713dd027
Piiiiiii <390049717@qq.com>
2022-01-04 13:03:25 -0800

    init

---
+++ src/sort.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	if len(os.Args) != 3 {
+		log.Fatalf("Usage: %v inputfile outputfile\n", os.Args[0])
+	}
+
+	log.Printf("Sorting %s to %s\n", os.Args[1], os.Args[2])
+
+	 */
+
+	fmt.Println("j")
+	
+}
